b5032aa85a061fd585b28d0c916ab9402c7ef12e
Veselin Zanev <57451986+veskozanev@users.noreply.github.com>
2021-07-16 03:25:01 +0100

    Add files via upload

---
+++ precache-manifest.826c93520fa29314c7f3c97164ad4f9a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8dc0557adcd9015bac6817f0b66b87",
+    "url": "/NFT-test/index.html"
+  },
+  {
+    "revision": "ff734845baa4fcd0ad57",
+    "url": "/NFT-test/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "4015af2ef32e49be2b9a",
+    "url": "/NFT-test/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "ff734845baa4fcd0ad57",
+    "url": "/NFT-test/static/js/2.2d53eab9.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "/NFT-test/static/js/2.2d53eab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4015af2ef32e49be2b9a",
+    "url": "/NFT-test/static/js/main.cf7010fc.chunk.js"
+  },
+  {
+    "revision": "e2b5fb9a60d5d929f9a0",
+    "url": "/NFT-test/static/js/runtime-main.a40e8562.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/NFT-test/static/media/photo.d6cbd8e6.png"
+  }
+]);
